4ec111d3b4e93e81101da127b8b7c901dd6ea0a8
anlozor <anlozor@gmail.com>
2026-03-31 18:57:01 +0200

    Creación archivos de rutas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
-// Núcleo del backend
+// Núcleo del backend, aquí es donde se va a coordinar todo
 // Estructura:
 // 1. Librerías importadas
 // 2. Servidor
 // 3. Middlewares
-// 4. Rutas
-// 5. Puerto
-// 6. Conexión BBDD
+// 4. Conexión BBDD
+// 5. Rutas
+// 6. Puerto
 // 7. Arrancar servidor
 
 // 1. Librerías
@@ -14,25 +14,25 @@ const cors = require('cors'); // Para poder comunicar el frontend con el backend
 require('dotenv').config(); // Para poder cargar las variables del archivo .env
 
 // 2. Servidor
-const servidor = express();
+const app = express();
 
 // 3. Middlewares
-servidor.use(express.json()); // Permitimos recibir datos en formato JSON
-servidor.use(cors()); // Permitimos hacer peticiones desde el frontend
+app.use(express.json()); // Permitimos recibir datos en formato JSON
+app.use(cors()); // Permitimos hacer peticiones desde el frontend
 
-// 4. Rutas
+// 4. Conexión a BBDD
+
+// 5. Rutas
 // Ruta de prueba
-servidor.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('API funcionando');
 });
 
-// 5. Definimos el puerto
+// 6. Definimos el puerto
 const PORT = process.env.PORT || 3000; // Por si falla .env
-
-// 6. Conexión a BBDD
 
 // 7. Arrancar el servidor
 // Arrancamos el servidor y mandamos un mensaje a la terminal para comprobar que funciona
-servidor.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor funcionando en puerto ${PORT}`);
 });
