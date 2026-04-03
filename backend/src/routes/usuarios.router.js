1ce71942ad3944c94dee1685e465f87132aee98c
anlozor <anlozor@gmail.com>
2026-04-03 12:47:23 +0200

    post simple en router de usuario

--- backend/src/routes/usuarios.router.js
+++ backend/src/routes/usuarios.router.js
@@ -6,16 +6,41 @@ const router = express.Router();
 
 // 3. Rutas
 // GET -> obtener listado de usuarios
-router.get();
+router.get('/', (req, res) => {
+    return res.send('Listado de usuarios');
+});
 
 // GET -> obtener un usuario concreto
-//router.get();
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    return res.send(`Usuario con id ${id}`);
+});
 
 // POST -> añadir un usuario nuevo
-//router.post();
+router.post('/', (req, res) => {
+    const {nombre} = req.body;
+
+    if (!nombre) {
+        return res.status(400).send();
+    }
+    // Se puede hacer una validación para comprobar que el usuario no existe ya en la BD(por ejemplo con el nombre)
+
+    const nuevo_usuario = {nombre};
+    return res.status(201).send(nuevo_usuario);
+});
 
 // PATCH -> actualizar datos de usuario
-//router.patch();
+//router.patch('/:id', (req, res) => {
+//    const {nombre} = req.body;
+//    if (!nombre) {
+//        return res.status(400).send();
+//    }
+    // Ahora tendríamos que buscar al usuario en la bd
+    // Comprobar que hay usuario y sino mandar un 404
+    // Y luego actualizar los campos
+    // Y termiar con un res.send();
+//});
 
 // 4. Exportar
 module.exports = router;
