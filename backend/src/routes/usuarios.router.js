080334cc379dee367e731d4ed35feb5a8fa28a40
anlozor <anlozor@gmail.com>
2026-04-24 13:57:29 +0200

    post de usuarios.router

--- backend/src/routes/usuarios.router.js
+++ backend/src/routes/usuarios.router.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         // Primero la query
-        const result = await pool.query('SELECT * FROM usuario');
+        const result = await pool.query(`SELECT * FROM usuario`);
         // Estado de petición HTTP y resultado
         res.status(200).json(result.rows);
     } catch (error) {
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
         // Leemos el id
         const id = req.params.id;
         // Hacemos la query
-        const result = await pool.query('SELECT * FROM usuario WHERE id_usuario = $1', [id]);
+        const result = await pool.query(`SELECT * FROM usuario WHERE id_usuario = $1`, [id]);
         // Comprobamos que nos ha devuelto algo y sino devolvemos error
         if (result.rows.length === 0) {
             return res.status(404).send('Usuario no encontrado');
@@ -43,16 +43,31 @@ router.get('/:id', async (req, res) => {
 });
 
 // POST -> añadir un usuario nuevo
-router.post('/', (req, res) => {
-    const {nombre} = req.body;
-
-    if (!nombre) {
-        return res.status(400).send();
+router.post('/', async (req, res) => {
+    try {
+        // Primero obtenemos lo que tiene que llevar el body para crear un nuevo usuario
+        const {nombre, email, contraseña, rol_id, alias} = req.body;
+    
+        // Comprobación por si algo falta
+        if (!nombre || !email || !contraseña || !rol_id || !alias) {
+            return res.status(400).send('Faltan datos obligatorios');
+        }
+        // Se puede hacer una validación para comprobar que el usuario no existe ya en la BD(por ejemplo con el nombre)
+        const existeNombre = await pool.query(`SELECT 1 FROM usuario WHERE nombre = $1`, [nombre]);
+        if (existeNombre.rows.length > 0) {
+            return res.status(409).send('Ya existe un usuario con el mismo nombre');
+        }
+    
+        // Hacemos la inserción del nuevo usuario
+        const result = await pool.query(`INSERT INTO usuario (nombre, email, contraseña, rol_id, alias, fecha_registro) 
+            VALUES ($1, $2, $3, $4, $5, CURRENT_DATE)
+            RETURNING *`, [nombre, email, contraseña, rol_id, alias]);
+        // Devolvemos el estado HTTP y la información
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error creando usuario:', error);
+        res.status(500).send('Error al crear usuario');
     }
-    // Se puede hacer una validación para comprobar que el usuario no existe ya en la BD(por ejemplo con el nombre)
-
-    const nuevo_usuario = {nombre};
-    return res.status(201).send(nuevo_usuario);
 });
 
 // PATCH -> actualizar datos de usuario
