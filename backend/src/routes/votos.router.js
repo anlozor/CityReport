76647dd1dcdc7e977216063e12f7f016a4bec5d3
anlozor <anlozor@gmail.com>
2026-04-24 12:30:02 +0200

    conexión con base de datos y prueba de votos.router

--- backend/src/routes/votos.router.js
+++ backend/src/routes/votos.router.js
@@ -1,5 +1,5 @@
 // Estructura del router:
-// 1. Importar Express
+// 1. Importar Express y conexión con PostgreSQL
 // 2. Crear router
 // Añadir middlewares si fueran necesarios
 // 3. Definir rutas:
@@ -7,8 +7,9 @@
 //      POST /votos
 // 4. Exportar router
 
-// 1. Express
+// 1. Express y PostgreSQL
 const express = require('express');
+const pool = require('../bd/bd');
 
 // 2. Router
 const router = express.Router();
@@ -20,12 +21,42 @@ const router = express.Router();
 
 // 3. Rutas
 // Obtener los votos
-router.get('/', (req, res) => {
-    return res.send('Listado de votos');
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM voto');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error obteniendo votos:', error);
+        res.status(500).send('Error al obtener votos');
+    }
 });
 
 // Crear voto
-//router.post();
+router.post('/', async (req, res) => {
+    try {
+        const {usuario_id, incidencia_id} = req.body;
+
+        // Control de error si alguna de las tres no está en el body
+        if (!usuario_id || !incidencia_id) {
+            return res.status(400).send('Faltan datos obligatorios');
+        }
+
+        const result = await pool.query(
+            `INSERT INTO voto (usuario_id, incidencia_id, fecha_voto)
+            VALUES ($1, $2, CURRENT_DATE)
+            RETURNING *`, [usuario_id, incidencia_id]
+        );
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+        console.error('Error creando voto:', error);
+        //res.status(500).send('Error al crear voto');
+        res.status(500).json({
+            message: 'Error al crear voto',
+            detail: error.message
+        });
+    }
+});
 
 // 4. Exportar
 module.exports = router;
