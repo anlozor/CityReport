45c5374c7e74df0c820ba9f2b3e3795d0df527c9
anlozor <anlozor@gmail.com>
2026-04-29 19:17:09 +0200

    Nueva versión del patch de usuarios

--- backend/src/routes/usuarios.router.js
+++ backend/src/routes/usuarios.router.js
@@ -78,23 +78,54 @@ router.post('/', async (req, res) => {
 router.patch('/:id', async (req, res) => {
     try {
         const id = req.params.id; // Leemos el id
-        /*************PATCH FIJO ORIGINAL*************/
-        // Leemos los datos del body
-        const {nombre, email, contraseña, rol_id, alias} = req.body;
-        // Hasheamos contraseña
-        const contraseñaHashed = await bcrypt.hash(contraseña, 10);
         // Buscamos al usuario en la bd para comprobar que existe
         const existe = await pool.query(`SELECT * FROM usuario WHERE id_usuario = $1`, [id]);
         // Comprobamos que existe el usuario
         if (existe.rows.length === 0) {
             return res.status(404).send('El usuario no existe');
         }
-        // Actualizamos los campos, en nuestro caso solo hemos hablado de cambiar la contraseña
-        // Pero está preparado para actualizar el resto de campos
-        const result = await pool.query(`UPDATE usuario SET contraseña = $1, alias = $3 WHERE id_usuario = $2 RETURNING *`,
-            [contraseñaHashed, id, alias]);
+
+        /*************PATCH FIJO ORIGINAL*************/
+        // Leemos los datos del body
+        //const {nombre, email, contraseña, rol_id, alias} = req.body;
+        // Hasheamos contraseña
+        //const contraseñaHashed = await bcrypt.hash(contraseña, 10);
+        // Actualizamos los campos
+        //const result = await pool.query(`UPDATE usuario SET contraseña = $1, alias = $3 WHERE id_usuario = $2 RETURNING *`,
+        //    [contraseñaHashed, id, alias]);
         // Devolvemos estado de HTTP y resultado
+        //res.status(200).json(result.rows[0]);
+
+        /*************PATCH DINÁMICO*************/
+        // Con esta versión no es necesario mandar todos los campos de usuario, sólo los que queremos actualizar
+        // Datos es un objeto para leer los objetos que vengan en el body
+        const datos = req.body;
+        // Campos es un array donde se va a guardar los campos del body para la query
+        const campos = [];
+        // Valores es un array donde se va a guardar los valores de los campos para la query
+        const valores = [];
+        // La idea es construirlo de manera que los campos queden como [nombre = $1, email = $2] 
+        // y los valores queden como [Andrea, andrea@ejemplo.com]
+        let contador = 1;
+        for (let campo in datos) {
+            // Comprobamos si el campo que estamos leyendo de datos es contraseña para hashearla
+            if (campo === 'contraseña') {
+                datos[campo] = await bcrypt.hash(datos[campo], 10);
+            }
+
+            campos.push(`${campo} = $${contador}`);
+            valores.push(datos[campo]);
+            contador++; // Utilizamos el contador para indicar el $1, $2, etc. Ya que cada vuelta del for es un valor de datos, es decir, un campo
+            // Además, nos viene bien para dejar el array valores ya colocado en orden
+        }
+        // Añadimos el id del usuario
+        valores.push(id);
+        // Montamos la query
+        const query = `UPDATE usuario SET ${campos.join(', ')} WHERE id_usuario = $${contador} RETURNING *`;
+        // Hacemos la consulta
+        const result = await pool.query(query, valores);
         res.status(200).json(result.rows[0]);
+
     } catch (error) {
         console.error('Error al actualizar usuario:', error);
         res.status(500).send('Error al actualizar usuario');
